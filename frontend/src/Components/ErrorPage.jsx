9c250bd8fb77836df0f083af18a3cef501c985c7
sovuixJob <sovuix@gmail.com>
2025-11-21 17:13:20 +0300

    add link HomePage

--- frontend/src/Components/ErrorPage.jsx
+++ frontend/src/Components/ErrorPage.jsx
@@ -1,22 +1,26 @@
 import Header from './Header/Header';
 
 const ErrorPage = () => {
-    return (
-        <div className="h-100 bg-light">
-            <div className="h-100" id="chat">
-                <div className="d-flex flex-column h-100">
-                    <Header />
-                    <div className="text-center">
-                        <img
-                            alt="Страница не найдена"
-                            className="img-fluid h-25"
-                            src='./public/404.svg'
-                        />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="h-100 bg-light">
+      <div className="h-100" id="chat">
+        <div className="d-flex flex-column h-100">
+          <Header />
+          <div className="text-center">
+            <img
+              alt="Страница не найдена"
+              className="img-fluid h-25"
+              src="./public/404.svg"
+            />
+            <h1 className="h4 text-muted">Страница не найдена</h1>
+            <p className="text-muted">
+              Но вы можете перейти <a href="/">на главную страницу сайта </a>
+            </p>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ErrorPage;
