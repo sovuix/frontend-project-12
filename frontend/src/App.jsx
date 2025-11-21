ffe0390abea9b43abe8d8e5a8812580ee86e746c
sovuixJob <sovuix@gmail.com>
2025-11-21 17:12:47 +0300

    add HomePage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,52 +2,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './Components/LoginPage';
 import ErrorPage from './Components/ErrorPage';
-
-// import CardBody from './Components/CardBody/CardBody';
-// import SignUpForm from './Components/SignupForm/SignupForm';
-// import Button from './Components/Button/Button';
-// import Container from './Components/Container/Container';
-// import Card from './Components/Card/Card';
-// import Logo from './Components/CardLogo/CardLogo';
-// import CardForm from './Components/CardForm/CardForm';
-// import Header from './Components/Header/Header';
-// import CardFooter from './Components/CardFooter/CardFooter';
+import HomePage from './Components/HomePage';
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="login" element={<LoginPage />} />
-                <Route path="*" element={<ErrorPage />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-//   <div className="h-100 bg-light">
-//     <div className="h-100" id="chat">
-//       <div className="d-flex flex-column h-100">
-//         <Header />
-//         <Container>
-//           <Card>
-//             <CardBody>
-//               <Logo />
-//               <CardForm>
-//                 <SignUpForm>
-//                   <Button
-//                     text="Войти"
-//                     className="w-100 mb-3 btn btn-outline-primary"
-//                   />
-//                 </SignUpForm>
-//               </CardForm>
-//             </CardBody>
-//             <CardFooter />
-//           </Card>
-//         </Container>
-//       </div>
-//     </div>
-//   </div>
-// );
-// }
 
 export default App;
