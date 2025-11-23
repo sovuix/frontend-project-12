dbfc1700b2e321b2226fb47dc4f466a871d93604
sovuix <sovuix@gmail.com>
2025-11-24 00:43:19 +0300

    edit to formik

--- frontend/src/Components/SignupForm/SignupForm.jsx
+++ frontend/src/Components/SignupForm/SignupForm.jsx
@@ -1,96 +1,100 @@
 import { useFormik } from 'formik';
 import './SignupForm.css';
-import { useState } from 'react';
+// import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const SignUpForm = ({ children }) => {
-  const navigate = useNavigate();
-  const [hasError, setHasError] = useState(false);
-  const getToken = async (username, password) => {
-    try {
-      const response = await fetch('/api/v1/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
+    const navigate = useNavigate();
+    // const [hasError, setHasError] = useState(false);
+    const getToken = async (username, password) => {
+        try {
+            const response = await fetch('/api/v1/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ username, password }),
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const data = await response.json();
+            return data.token;
+        } catch (error) {
+            console.error('Error getting token:', error);
+            throw error;
+        }
+    };
+
+    const formik = useFormik({
+        initialValues: {
+            userLogin: '',
+            password: '',
+            hasError: false,
         },
-        body: JSON.stringify({ username, password }),
-      });
+        onSubmit: async (values, { setSubmitting, setValues }) => {
+            try {
+                console.log(values);
+                const token = await getToken(values.userLogin, values.password);
+                localStorage.setItem('jwtToken', token);
+                navigate('/');
+            } catch (error) {
+                setValues({ hasError: true });
+                console.error('Login failed:', error);
+            } finally {
+                setSubmitting(false);
+            }
+        },
+    });
 
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
+    console.log(formik);
 
-      const data = await response.json();
-      return data.token;
-    } catch (error) {
-      console.error('Error getting token:', error);
-      throw error;
-    }
-  };
+    return (
+        <form
+            className="col-12 col-md-6 mt-3 mt-md-0 w-100"
+            onSubmit={formik.handleSubmit}
+        >
+            <div className="form-floating mb-3">
+                <input
+                    className={`form-control ${
+                        formik.values.hasError ? 'is-invalid' : ''
+                    }`}
+                    id="userLogin"
+                    name="userLogin"
+                    type="text"
+                    onChange={formik.handleChange}
+                    value={formik.values.userLogin}
+                    required
+                    placeholder="Ваш ник"
+                />
+                <label htmlFor="userLogin">Ваш ник</label>
+            </div>
 
-  const formik = useFormik({
-    initialValues: {
-      userLogin: '',
-      password: '',
-    },
-    onSubmit: async (values, { setSubmitting }) => {
-      try {
-        console.log(values);
-        const token = await getToken(values.userLogin, values.password);
-        localStorage.setItem('jwtToken', token);
-        console.log('Token saved successfully');
-        setHasError(false);
-        navigate('/');
-      } catch (error) {
-        setHasError(true);
-        console.error('Login failed:', error);
-        console.log(hasError);
-      } finally {
-        setSubmitting(false);
-      }
-    },
-  });
-
-  return (
-    <form
-      className="col-12 col-md-6 mt-3 mt-md-0 w-100"
-      onSubmit={formik.handleSubmit}
-    >
-      <div className="form-floating mb-3">
-        <input
-          className={`form-control ${hasError ? 'is-invalid' : ''}`}
-          id="userLogin"
-          name="userLogin"
-          type="text"
-          onChange={formik.handleChange}
-          value={formik.values.userLogin}
-          required
-          placeholder="Ваш ник"
-        />
-        <label htmlFor="userLogin">Ваш ник</label>
-      </div>
-
-      <div className="form-floating mb-4">
-        <input
-          className={`form-control ${hasError ? 'is-invalid' : ''}`}
-          id="password"
-          name="password"
-          type="password"
-          onChange={formik.handleChange}
-          value={formik.values.password}
-          required
-          placeholder="Пароль"
-        />
-        {hasError && (
-          <div className="invalid-tooltip">
-            Неверные имя пользователя или пароль
-          </div>
-        )}
-        <label htmlFor="password">Пароль</label>
-      </div>
-      {children}
-    </form>
-  );
+            <div className="form-floating mb-4">
+                <input
+                    className={`form-control ${
+                        formik.values.hasError ? 'is-invalid' : ''
+                    }`}
+                    id="password"
+                    name="password"
+                    type="password"
+                    onChange={formik.handleChange}
+                    value={formik.values.password}
+                    required
+                    placeholder="Пароль"
+                />
+                {formik.values.hasError && (
+                    <div className="invalid-tooltip">
+                        Неверные имя пользователя или пароль
+                    </div>
+                )}
+                <label htmlFor="password">Пароль</label>
+            </div>
+            {children}
+        </form>
+    );
 };
 
 export default SignUpForm;
