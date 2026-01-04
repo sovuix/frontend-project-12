a6acf6cb1be82c4786add658e37ecd711e59f031
sovuix <sovuix@gmail.com>
2026-01-04 15:46:13 +0300

    add slice

---
+++ frontend/src/state/slices/channelsSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const channelsSlice = createSlice({
+    name: 'chat',
+    initialState: {
+        channels:[],
+        loading: false,
+        error: null,
+    },
+    reducers: {
+        setChannels: (state, action) => {
+            state.channels = action.payload;
+            state.loading = false;
+            state.error = null
+        },
+        setLoading: (state) => {
+            state.loading = true;
+            state.error = null;
+        },
+        setError: (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+        }
+    }
+})
+
+export const {setChannels, setLoading, setError} = channelsSlice.actions;
+export  default channelsSlice.reducer;
