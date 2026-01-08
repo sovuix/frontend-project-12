24da8365542a5f556c968308976098afdd54b1fe
sovuix <sovuix@gmail.com>
2026-01-08 15:22:58 +0300

    add MessagesHeader

---
+++ frontend/src/Components/ChatPanel/MessagesHeader/MessagesHeader.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import {selectMessagesCountByChannelId} from '../../../state/slices/messagesSlice'
+
+const MessagesHeader = () => {
+    const currentChannelId = useSelector(
+        (state) => state.chat.currentChannelId
+    );
+    const channels = useSelector((state) => state.chat.channels);
+    const currentChannel = channels.find((ch) => ch.id === currentChannelId);
+
+    const countMessages = useSelector(selectMessagesCountByChannelId(currentChannelId))
+
+    return (
+        channels.length && (
+            <div className="bg-light mb-4 p-3 shadow-sm small">
+                <p className="m-0">
+                    <b># {currentChannel.name}</b>
+                </p>
+                <span className='text-muted'>{countMessages} сообщений</span> 
+            </div>
+        )
+    );
+};
+
+export default MessagesHeader;
