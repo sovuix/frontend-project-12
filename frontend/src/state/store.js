0ed7a316972abe0e604f4f286611cd8de3690f0b
sovuix <sovuix@gmail.com>
2026-01-08 15:25:21 +0300

    add messagesReducer

--- frontend/src/state/store.js
+++ frontend/src/state/store.js
@@ -1,9 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit';
 import channelsReducer from './slices/channelsSlice'
+import messagesReducer from './slices/messagesSlice'
 
 const store = configureStore({
     reducer: {
         chat: channelsReducer,
+        messages: messagesReducer,
     }
 })
 
