c03fdd910842eacfafccafa632df22e1a1434638
sovuix <sovuix@gmail.com>
2026-01-10 18:16:53 +0300

    add components

--- frontend/src/Components/ChatPanel/ChatPanel.jsx
+++ frontend/src/Components/ChatPanel/ChatPanel.jsx
@@ -1,10 +1,14 @@
 import MessagesHeader from './MessagesHeader/MessagesHeader';
+import MessagesList from './MessagesList/MessagesList';
+import MessageForm from './MessageForm/MessageForm';
 
 const ChatPanel = () => {
     return (
         <div className="col p-0 h-100">
             <div className="d-flex flex-column h-100">
                 <MessagesHeader/>
+                <MessagesList/>
+                <MessageForm/>
             </div>
         </div>
     );
