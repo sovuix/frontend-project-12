7d8d3c188efdaa4e70fcb7841acd7559ab2bd603
sovuix <sovuix@gmail.com>
2026-01-11 14:51:40 +0300

    add authSlice

---
+++ frontend/src/state/slices/authSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState: {
+        id: null,
+        username: '',
+        token: null,
+    },
+    reducers: {
+        setUser: (state, action) => {
+            state.id = action.payload.id;
+            state.username = action.payload.username;
+            state.token = action.payload.token;
+        },
+        clearUser:(state) => {
+            state.id = null;
+            state.username = '';
+            state.token = null;
+        }
+    }
+})
+
+export const {setUser, clearUser} = authSlice.actions;
+export default authSlice.reducer;
