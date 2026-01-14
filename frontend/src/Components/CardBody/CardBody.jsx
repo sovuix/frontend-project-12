12f0201a36d264bbc4e81203d435d9aeee4ea868
sovuix <sovuix@gmail.com>
2026-01-14 21:59:38 +0300

    fixed with className

--- frontend/src/Components/CardBody/CardBody.jsx
+++ frontend/src/Components/CardBody/CardBody.jsx
@@ -1,7 +1,12 @@
 import './CardBody.css';
+import cn from 'classnames'
 
-const CardBody = ({ children }) => {
-  return <div className="cb card-body row p-5">{children}</div>;
+const CardBody = ({ children, className, variant = 'login' }) => {
+  const baseClasses = {
+    login: 'cb card-body row p-5',
+    signUp: 'card-body d-flex flex-column flex-md-row justify-content-around align-items-center p-5'
+  }
+  return <div className={cn(baseClasses[variant], className)}>{children}</div>;
 };
 
 export default CardBody;
--- frontend/src/Components/CardLogo/CardLogo.jsx
+++ frontend/src/Components/CardLogo/CardLogo.jsx
@@ -1,7 +1,34 @@
-const Logo = () => {
+// const Logo = () => {
+//   return (
+//     <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
+//       <img src="./public/chat-logo.jpg" className="rounded-circle"></img>
+//     </div>
+//   );
+// };
+
+// export default Logo;
+import cn from 'classnames'
+const Logo = ({ className, variant = 'login' }) => {
+  const variants = {
+    login: {
+      src: '/chat-logo.jpg',
+      classes: 'col-12 col-md-6 d-flex align-items-center justify-content-center'
+    },
+    signUp: {
+      src: '/signup-logo.jpg',
+      classes: ''
+    }
+  };
+
+  const { src, classes } = variants[variant];
+
   return (
-    <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
-      <img src="./public/chat-logo.jpg" className="rounded-circle"></img>
+    <div className={cn(classes, className)}>
+      <img 
+        src={src} 
+        className="rounded-circle"
+        alt="App logo"
+      />
     </div>
   );
 };
