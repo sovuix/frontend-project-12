b689abad11a6092e4bb829554d710c81a1ce04d9
sovuix <sovuix@gmail.com>
2026-01-18 16:49:32 +0300

    add host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,12 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5002,
+    host:true,
     proxy: {
-      // Проксируем запросы к API
       '/api': {
         target: 'http://localhost:5001',
       },
-      // Проксируем WebSocket соединения
       '/socket.io': {
         target: 'ws://localhost:5001',
         ws: true,
