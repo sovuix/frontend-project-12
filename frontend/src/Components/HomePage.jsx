0f5570cf83789c6ff02bf2308cde0790269f5040
sovuix <sovuix@gmail.com>
2026-01-19 17:11:05 +0300

    add navigate '/login'

--- frontend/src/Components/HomePage.jsx
+++ frontend/src/Components/HomePage.jsx
@@ -1,124 +1,134 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import {
-    setLoading,
-    setError,
-    setChannels,
-} from '../state/slices/channelsSlice';
-import Header from './Header/Header';
-import ChannelsPanel from './ChannelsPanel/ChannelsPanel';
-import ChatPanel from './ChatPanel/ChatPanel';
+  setLoading,
+  setError,
+  setChannels,
+} from "../state/slices/channelsSlice";
+import Header from "./Header/Header";
+import ChannelsPanel from "./ChannelsPanel/ChannelsPanel";
+import ChatPanel from "./ChatPanel/ChatPanel";
 
-import socket from '../services/socket';
-import { addMessage } from '../state/slices/messagesSlice';
-import { setMessages } from '../state/slices/messagesSlice';
-import {setUser} from '../state/slices/authSlice'
+import socket from "../services/socket";
+import { addMessage } from "../state/slices/messagesSlice";
+import { setMessages } from "../state/slices/messagesSlice";
+import { setUser } from "../state/slices/authSlice";
+
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        console.log('Начинаем слушать WebSocket...');
+  useEffect(() => {
+    const token = localStorage.getItem("jwtToken");
+    if (!token) {
+      navigate("/login");
+    }
+  }, [navigate]);
 
-        const handleNewMessage = (message) => {
-            console.log('Получено через WebSocket:', message);
-            dispatch(addMessage(message));
-        };
+  useEffect(() => {
+    console.log("Начинаем слушать WebSocket...");
 
-        socket.on('newMessage', handleNewMessage);
+    const handleNewMessage = (message) => {
+      console.log("Получено через WebSocket:", message);
+      dispatch(addMessage(message));
+    };
 
-        return () => {
-            socket.off('newMessage', handleNewMessage);
-        };
-    }, [dispatch]);
+    socket.on("newMessage", handleNewMessage);
 
-    const { channels, loading, error } = useSelector((state) => state.chat);
+    return () => {
+      socket.off("newMessage", handleNewMessage);
+    };
+  }, [dispatch]);
 
-    useEffect(() => {
-        const fetchChannels = async () => {
-            try {
-                dispatch(setLoading());
-                const token = localStorage.getItem('jwtToken');
+  const { channels, loading, error } = useSelector((state) => state.chat);
 
-                const response = await fetch('/api/v1/channels', {
-                    method: 'GET',
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                        'Content-Type': 'application/json',
-                    },
-                });
+  useEffect(() => {
+    const fetchChannels = async () => {
+      try {
+        dispatch(setLoading());
+        const token = localStorage.getItem("jwtToken");
 
-                if (!response.ok) {
-                    throw new Error('Ошибка при загрузке каналов');
-                }
+        const response = await fetch("/api/v1/channels", {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        });
 
-                const data = await response.json();
-                dispatch(setChannels(data));
-            } catch (error) {
-                dispatch(setError(error));
-                console.error('Error', error);
-            }
-        };
-        fetchChannels();
-    }, [dispatch]);
+        if (!response.ok) {
+          throw new Error("Ошибка при загрузке каналов");
+        }
 
-    useEffect(() => {
-        const fetchMessages = async () => {
-            try {
-                const token = localStorage.getItem('jwtToken');
-                const response = await fetch('/api/v1/messages', {
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                        'Content-Type': 'application/json',
-                    },
-                });
+        const data = await response.json();
+        dispatch(setChannels(data));
+      } catch (error) {
+        dispatch(setError(error));
+        console.error("Error", error);
+      }
+    };
+    fetchChannels();
+  }, [dispatch]);
 
-                if (response.ok) {
-                    const data = await response.json();
-                    dispatch(setMessages(data));
-                }
-            } catch (err) {
-                console.error('Ошибка загрузки сообщений:', err);
-            }
-        };
+  useEffect(() => {
+    const fetchMessages = async () => {
+      try {
+        const token = localStorage.getItem("jwtToken");
+        const response = await fetch("/api/v1/messages", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        });
 
-        fetchMessages();
-    }, [dispatch]);
+        if (response.ok) {
+          const data = await response.json();
+          dispatch(setMessages(data));
+        }
+      } catch (err) {
+        console.error("Ошибка загрузки сообщений:", err);
+      }
+    };
 
-    useEffect(() => {
-    const token = localStorage.getItem('jwtToken');
-    const username = localStorage.getItem('username');
-    
+    fetchMessages();
+  }, [dispatch]);
+
+  useEffect(() => {
+    const token = localStorage.getItem("jwtToken");
+    const username = localStorage.getItem("username");
+
     if (token && username) {
-        dispatch(setUser({ username, token }));
+      dispatch(setUser({ username, token }));
     }
-}, [dispatch]);
+  }, [dispatch]);
 
-    if (loading) {
-        return <div>Загрузка каналов...</div>;
-    }
+  if (loading) {
+    return <div>Загрузка каналов...</div>;
+  }
 
-    if (error) {
-        return <div>Ошибка: {error}</div>;
-    }
+  if (error) {
+    return <div>Ошибка: {error}</div>;
+  }
 
-    console.log(channels);
+  console.log(channels);
 
-    return (
-        <div className="h-100 bg-light">
-            <div className="h-100" id="chat">
-                <div className="d-flex flex-column h-100">
-                    <Header />
-                    <div className="container h-100 my-4 overflow-hidden rounded shadow">
-                        <div className="row h-100 bg-white flex-md-row">
-                            <ChannelsPanel />
-                            <ChatPanel />
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="h-100 bg-light">
+      <div className="h-100" id="chat">
+        <div className="d-flex flex-column h-100">
+          <Header />
+          <div className="container h-100 my-4 overflow-hidden rounded shadow">
+            <div className="row h-100 bg-white flex-md-row">
+              <ChannelsPanel />
+              <ChatPanel />
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
