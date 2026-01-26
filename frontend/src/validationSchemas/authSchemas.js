bfbb4fc92fb8ea64e2726ec12e8cb0ae8efe57f3
sovuix <sovuix@gmail.com>
2026-01-26 21:17:21 +0300

    add rules for ModalWindow

--- frontend/src/validationSchemas/authSchemas.js
+++ frontend/src/validationSchemas/authSchemas.js
@@ -1,17 +1,32 @@
 import * as yup from 'yup';
 
 export const registrationSchema = yup.object({
-    username: yup.string()
+    username: yup
+        .string()
         .trim()
         .required('Обязательное поле')
         .min(3, 'Не менее 3 символов')
         .max(50, 'Не более 20 символов'),
-    
-    password: yup.string()
+
+    password: yup
+        .string()
         .required('Обязательное поле')
         .min(6, 'Не менее 6 символов'),
 
-    confirmPassword: yup.string()
+    confirmPassword: yup
+        .string()
         .required('Подтвердите пароль')
-        .oneOf([yup.ref('password'), null], 'Пароли не совпадают') // сравниваем с password
+        .oneOf([yup.ref('password'), null], 'Пароли не совпадают'), // сравниваем с password
 });
+
+export const createModalSchema = (existingNames = []) => {
+    yup.object({
+        channelname: yup
+            .string()
+            .trim()
+            .required('Обязательное поле')
+            .min(3, 'Не менее 3 символов')
+            .max(50, 'Не более 20 символов')
+            .notOneOf(existingNames, 'Название должно быть уникальным'),
+    });
+};
