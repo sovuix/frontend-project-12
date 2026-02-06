9c2cf86c15fc5b63480c2ec92de4d0da338f132f
sovuix <sovuix@gmail.com>
2026-02-06 07:00:33 +0300

    draft

---
+++ frontend/src/i18n/index.js
@@ -0,0 +1,22 @@
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import enTranslations from './locales/en/common.json';
+import ruTranslations from './locales/ru/common.json';
+
+export const createI18nInstance = async () => {
+  const instance = i18next.createInstance();
+  
+  await instance
+    .use(initReactI18next)
+    .init({
+      fallbackLng: 'en',
+      defaultNS: 'common',
+      resources: {
+        en: { common: enTranslations },
+        ru: { common: ruTranslations }
+      }
+    });
+  
+  return instance;
+};
