e172d5e9d835b47f5e6ffe7f577ae248b9f70d83
sovuix <sovuix@gmail.com>
2026-02-08 23:17:49 +0300

    fixed i18n

--- frontend/src/Components/ChannelsPanel/ChannelsPanel.jsx
+++ frontend/src/Components/ChannelsPanel/ChannelsPanel.jsx
@@ -108,7 +108,9 @@ const ChannelsPanel = () => {
     <>
       <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
         <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
-          <b>{t("channels")}</b>
+          {/* <b>{t("channels")}</b> */}
+          <b>{t("common.channels")}</b>
+
           <Button
             onClick={handleOpenAddModal}
             className="p-0 text-primary btn btn-group-vertical"
