ab8419f365936e19ba5f7af5734c0c3edda7c78d
sovuix <sovuix@gmail.com>
2026-02-11 15:19:52 +0300

    add Rollbar

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,16 +6,23 @@ import { createI18nInstance } from "./i18n";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
+import { rollbarConfig } from "./services/rollbar.js";
+import { Provider as RollbarProvider, ErrorBoundary } from "@rollbar/react";
+
 const initApp = async () => {
   const i18nInstance = await createI18nInstance();
   const root = createRoot(document.getElementById("root"));
   root.render(
     <StrictMode>
-      <Provider store={store}>
-        <I18nextProvider i18n={i18nInstance}>
-          <App />
-        </I18nextProvider>
-      </Provider>
+      <RollbarProvider config={rollbarConfig}>
+        <ErrorBoundary>
+          <Provider store={store}>
+            <I18nextProvider i18n={i18nInstance}>
+              <App />
+            </I18nextProvider>
+          </Provider>
+        </ErrorBoundary>
+      </RollbarProvider>
     </StrictMode>,
   );
 };
---
+++ frontend/src/services/rollbar.js
@@ -0,0 +1,4 @@
+export const rollbarConfig = {
+  accessToken: "82e79f9439174c3c9531c76a9d840c1e",
+  environment: "testenv",
+};
