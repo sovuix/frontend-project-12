054fa20d5f247549cdf0b1b6a920b0530557442c
sovuix <sovuix@gmail.com>
2026-02-26 22:51:46 +0300

    fixed error style

--- frontend/src/Components/LoginForm/LoginForm.jsx
+++ frontend/src/Components/LoginForm/LoginForm.jsx
@@ -1,4 +1,4 @@
-import { useDispatch} from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { useFormik } from 'formik'
 import { useNavigate } from 'react-router-dom'
 import { setUser } from '../../state/slices/authSlice'
